0de6cbfd21c740de4743a42f018f8260e5ffc500
Yang Wenqiang <1106911190@qq.com>
2016-09-05 19:26:02 +0800

    delete some exe

--- _posts/software/pytesser/test.py
+++
@@ -1,8 +0,0 @@
-from  pytesser import *
-im1 = Image.open("code.png")
-text1 = image_to_string(im1)
-print text1
-print "\n"
-im2 = Image.open("fonts_test.png")
-text2 = image_to_string(im2)
-print text2
